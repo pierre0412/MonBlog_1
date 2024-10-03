96133f320078b699e52ee91f6a13895a1a456377
pierre <pierre@esteron.net>
2024-10-03 22:05:10 +0200

    création du modèle Article rangement des templates création de vue liste des articles

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+
+User = get_user_model()
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField("titre", max_length=100)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from blog.models import Article
+
+
 # Create your views here.
+def index(request):
+    articles_list = Article.objects.order_by('-created')
+    context = {'articles_list': articles_list}
+    return render(request, "blog/index.html", context)
